2e41e120afa5d619348659c829393be2a81b1c28
Krzysztof Kućmierz <krzysztof.kucmierz@sciencenet.online>
2025-09-22 12:45:17 +0200

    refactor: update project to TypeScript 2/2

--- App.tsx
+++ App.tsx
@@ -3,20 +3,25 @@ import Die from "./Die"
 import { nanoid } from "nanoid"
 import Confetti from "react-confetti"
 
-export default function App() {
-    const [dice, setDice] = useState(() => generateAllNewDice())
-    const buttonRef = useRef(null)
+import type { Die as DieType } from "./Die"
+import type { JSX } from "react"
 
-    const gameWon = dice.every(die => die.isHeld) &&
+export default function App():JSX.Element {
+    const [dice, setDice] = useState<DieType[]>(():DieType[] => generateAllNewDice())
+    const buttonRef = useRef<HTMLButtonElement>(null)
+
+    const gameWon: boolean = dice.every((die:DieType):boolean => die.isHeld) &&
         dice.every(die => die.value === dice[0].value)
         
     useEffect(() => {
         if (gameWon) {
-            buttonRef.current.focus()
+            if (buttonRef.current) {
+                buttonRef.current.focus()
+            }
         }
     }, [gameWon])
 
-    function generateAllNewDice() {
+    function generateAllNewDice(): DieType[] {
         return new Array(10)
             .fill(0)
             .map(() => ({
@@ -26,9 +31,9 @@ export default function App() {
             }))
     }
     
-    function rollDice() {
+    function rollDice():void {
         if (!gameWon) {
-            setDice(oldDice => oldDice.map(die =>
+            setDice((oldDice:DieType[]):DieType[] => oldDice.map((die:DieType):DieType =>
                 die.isHeld ?
                     die :
                     { ...die, value: Math.ceil(Math.random() * 6) }
@@ -38,20 +43,20 @@ export default function App() {
         }
     }
 
-    function hold(id) {
-        setDice(oldDice => oldDice.map(die =>
+    function hold(id: string):void {
+        setDice((oldDice:DieType[]):DieType[] => oldDice.map((die:DieType):DieType =>
             die.id === id ?
                 { ...die, isHeld: !die.isHeld } :
                 die
         ))
     }
 
-    const diceElements = dice.map(dieObj => (
+    const diceElements:JSX.Element[] = dice.map((dieObj:DieType):JSX.Element => (
         <Die
             key={dieObj.id}
             value={dieObj.value}
             isHeld={dieObj.isHeld}
-            hold={() => hold(dieObj.id)}
+            hold={():void => hold(dieObj.id)}
         />
     ))
 
--- Die.tsx
+++ Die.tsx
@@ -1,15 +1,29 @@
-export default function Die(props) {
+import { JSX } from "react";
+
+export type Die = {
+    value: number;
+    isHeld: boolean;
+    id: string;
+}
+
+type DieProps = {
+    value: number;
+    isHeld: boolean;
+    hold: () => void;
+}
+
+export default function Die({value, isHeld, hold}:DieProps): JSX.Element {
     const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "white"
+        backgroundColor: isHeld ? "#59E391" : "white"
     }
     
     return (
         <button 
             style={styles}
-            onClick={props.hold}
-            aria-pressed={props.isHeld}
-            aria-label={`Die with value ${props.value}, 
-            ${props.isHeld ? "held" : "not held"}`}
-        >{props.value}</button>
+            onClick={hold}
+            aria-pressed={isHeld}
+            aria-label={`Die with value ${value}, 
+            ${isHeld ? "held" : "not held"}`}
+        >{value}</button>
     )
 }
